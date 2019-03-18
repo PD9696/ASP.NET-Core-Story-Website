817e2e4ab26e40814136ca9dd1686151a37cdf57
Peter Dyczkowski <48145220+PD9696@users.noreply.github.com>
2019-03-18 15:25:21 -0700

    Add project files.

---
+++ WebApplication3/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication3.Models.Profile> Profile { get; set; }
+        public DbSet<WebApplication3.Models.FollowerList> FollowerList { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<FollowerList>()
+                .HasKey(FollowerList => new { FollowerList.ProfileId, FollowerList.FollowerId });
+
+            modelBuilder.Entity<LikeList>()
+                .HasKey(LikeList => new { LikeList.ProfileId, LikeList.StoryId });
+
+            modelBuilder.Entity<Profile>()
+                .HasIndex(u => u.UserName)
+                .IsUnique();
+        }
+
+        public DbSet<WebApplication3.Models.Story> Story { get; set; }
+        public DbSet<WebApplication3.Models.LikeList> LikeList { get; set; }
+    }
+}
